8009279ccd31b6316e40f6d75146a93ea9786ba3
Dam450 <68165752+dam450@users.noreply.github.com>
2023-01-21 00:47:30 -0300

    add routes index

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
 const express = require('express')
+const routes = require('./routes')
 
-const app = express()
 const PORT = 3333
+const app = express()
+app.use(express.json())
+app.use(routes)
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
 app.listen(PORT, () => {
   console.log(`[Server] listening on port ${PORT}`)
