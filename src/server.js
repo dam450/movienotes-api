4a08695b5372ed989c389a0bb8fd29818f85183e
Dam450 <68165752+dam450@users.noreply.github.com>
2023-01-23 20:36:51 -0300

    add error handling

--- src/server.js
+++ src/server.js
@@ -1,4 +1,7 @@
 const express = require('express')
+require('express-async-errors')
+
+const AppError = require('./utils/AppError')
 const routes = require('./routes')
 
 const PORT = 3333
@@ -6,6 +9,20 @@ const app = express()
 app.use(express.json())
 app.use(routes)
 
+app.use((error, request, response, next) => {
+
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: 'error',
+      message: error.message
+    })
+  }
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error'
+  })
+})
 
 app.listen(PORT, () => {
   console.log(`[Server] listening on port ${PORT}`)
