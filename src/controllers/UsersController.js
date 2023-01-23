21e73141c466353422e52feee3b9c6e57a40d1a2
Dam450 <68165752+dam450@users.noreply.github.com>
2023-01-23 20:38:07 -0300

    add error handling

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,8 +1,12 @@
+const AppError = require("../utils/AppError")
+
 class UsersController {
 
   create(req, res) {
 
     const { name, email, password } = req.body
+
+    if (!name || !email || !password) throw new AppError('Preencha dados obrigatórios')
 
     res.status(201).json({ name, email, password })
 
