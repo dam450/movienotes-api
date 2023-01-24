147a6c36eea2413d78325fbb8f3288c45467a2b6
Dam450 <68165752+dam450@users.noreply.github.com>
2023-01-24 16:08:26 -0300

    wip: update moviesController

--- src/controllers/MoviesController.js
+++ src/controllers/MoviesController.js
@@ -1,9 +1,19 @@
+const knex = require('../database/knex')
+
 class MoviesController {
 
-  async create(req, res)  {
+  async create(req, res) {
+
+    const { user_id } = req.params
+    const { title, description, rating } = req.body
+
+    /**
+     * @todo receber os dados e gravar no BD
+     */
 
 
-    res.json({ ...req.params, ...req.body })
+    res.json({ user_id, title, description, rating })
+
   }
 
 }
