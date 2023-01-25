065ab7595644148cc159698f3ef5fd33f61bc3b7
Dam450 <68165752+dam450@users.noreply.github.com>
2023-01-25 14:56:40 -0300

    update Docs

--- src/controllers/MoviesController.js
+++ src/controllers/MoviesController.js
@@ -1,21 +1,43 @@
+/**
+ * @typedef {{title: string, description: string,
+ * rating: number, tags: [string]}} movie
+ */
+
 const knex = require('../database/knex')
 
-class MoviesController {
+const AppError = require('../utils/AppError')
 
+class MoviesController {
   async create(req, res) {
 
     const { user_id } = req.params
-    const { title, description, rating } = req.body
 
-    /**
-     * @todo receber os dados e gravar no BD
-     */
+    /**@type movie */
+    const { title, description, rating, tags} = req.body
 
+    if (isNaN(rating)) throw new AppError('Valor de rating inválido.')
 
-    res.json({ user_id, title, description, rating })
+    const ratingValue = Number(rating)
 
+    if (ratingValue > 5 || ratingValue < 0)
+      throw new AppError('Informe valor de rating entre 1 e 5.')
+
+    const note_id = await knex('movie_notes').insert({
+      title,
+      description,
+      rating: ratingValue,
+      user_id
+    })
+
+    const hasTags = tags?.length
+
+    if (hasTags) {
+      const tagsInsert = tags.map(name => ({ note_id, user_id, name }))
+      await knex('movie_tags').insert(tagsInsert)
+    }
+
+    res.json({ user_id, title, description, ratingValue, tags })
   }
-
 }
 
 module.exports = MoviesController
