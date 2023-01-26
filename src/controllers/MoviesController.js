d456195403e0cdcc5248f1a6cffca1e7a53f7582
Dam450 <68165752+dam450@users.noreply.github.com>
2023-01-26 13:19:21 -0300

    add controller movies (delete, index, show)

--- src/controllers/MoviesController.js
+++ src/controllers/MoviesController.js
@@ -5,10 +5,9 @@ const AppError = require('../utils/AppError')
 class MoviesController {
 
   async create(req, res) {
+    //const { user_id } = req.params
 
-    const { user_id } = req.params
-
-    const { title, description, rating, tags} = req.body
+    const { title, description, rating, tags, user_id } = req.body
 
     if (isNaN(rating)) throw new AppError('Valor de rating inválido.')
 
@@ -21,7 +20,7 @@ class MoviesController {
       title,
       description,
       rating: ratingValue,
-      user_id
+      user_id,
     })
 
     const hasTags = tags?.length
@@ -32,6 +31,55 @@ class MoviesController {
     }
 
     res.json({ user_id, title, description, ratingValue, tags })
+  }
+
+  async update(req, res) {
+    /**
+     * @todo criar update de movies
+     */
+
+    console.count('UPDATE')
+    res.json({ ...req.params, ...req.body })
+  }
+
+  async delete(req, res) {
+
+    const { id } = req.params
+
+    const movieNote = await knex('movie_notes').where({ id }).delete()
+
+    if (!movieNote) return res.status(404).json()
+
+    return res.json({ id })
+  }
+
+  async index(req, res) {
+
+    const { user_id } = req.body
+
+    const movieNotes = await knex('movie_notes')
+      .where({ user_id })
+      .orderBy('created_at')
+
+    res.json([ ...movieNotes ])
+  }
+
+  async show(req, res) {
+
+    const { id } = req.params
+
+    const movieNote = await knex('movie_notes').select('*').where({ id }).first()
+
+    if (!movieNote) return res.status(404).end()
+
+    const movieTags = await knex('movie_tags').where({ note_id: id }).orderBy('name', 'asc')
+
+    let tags = []
+
+    if (movieTags)
+      tags = movieTags.map(tag => (tag.name))
+
+    res.json({ ...movieNote, tags })
   }
 }
 
