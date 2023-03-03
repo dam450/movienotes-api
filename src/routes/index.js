69d02571e644c05935a6e66e538b9d432ce75dba
Dam450 <68165752+dam450@users.noreply.github.com>
2023-03-03 15:11:49 -0300

    create sessions route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,9 @@
 const { Router } = require('express')
+
 const moviesRoutes = require('./movies.routes')
 const usersRoutes = require('./users.routes')
+const sessionsRoutes = require('./sessions.routes')
+
 
 const routes = Router()
 
@@ -10,6 +13,8 @@ routes.get('/', (req, res) => {
 
 routes.use('/users', usersRoutes)
 routes.use('/movies', moviesRoutes)
+routes.use('/sessions', sessionsRoutes)
+
 
 
 module.exports = routes
